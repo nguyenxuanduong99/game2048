9944e59668e118bb4df98d3c78de8dcc81fc58e7
Thuong1998 <37398323+Thuong1998@users.noreply.github.com>
2018-03-16 12:02:45 +0700

    Add files via upload

---
+++ introduce.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<conio.h>
+#include <windows.h>
+using namespace std;
+void introduction()
+{
+    cout<<"                        CHAO MUNG BAN DEN VOI TRO CHOI                        "<<endl;
+    cout<<"                                                                              "<<endl;
+    cout<<"   222222222222222         000000000            444444444       888888888     "<<endl;
+	cout<<"  2:::::::::::::::22     00:::::::::00         4::::::::4     88:::::::::88   "<<endl;
+	cout<<"  2::::::222222:::::2  00:::::::::::::00      4:::::::::4   88:::::::::::::88 "<<endl;
+	cout<<"  2222222     2:::::2 0:::::::000:::::::0    4::::44::::4  8::::::88888::::::8"<<endl;
+	cout<<"              2:::::2 0::::::0   0::::::0   4::::4 4::::4  8:::::8     8:::::8"<<endl;
+	cout<<"              2:::::2 0:::::0     0:::::0  4::::4  4::::4  8:::::8     8:::::8"<<endl;
+	cout<<"           2222::::2  0:::::0     0:::::0 4::::4   4::::4   8:::::88888:::::8 "<<endl;
+	cout<<"      22222::::::22   0:::::0 000 0:::::04::::444444::::444  8:::::::::::::8  "<<endl;
+	cout<<"    22::::::::222     0:::::0 000 0:::::04::::::::::::::::4 8:::::88888:::::8 "<<endl;
+	cout<<"   2:::::22222        0:::::0     0:::::04444444444:::::4448:::::8     8:::::8"<<endl;
+	cout<<"  2:::::2             0:::::0     0:::::0          4::::4  8:::::8     8:::::8"<<endl;
+	cout<<"  2:::::2             0::::::0   0::::::0          4::::4  8:::::8     8:::::8"<<endl;
+	cout<<"  2:::::2       2222220:::::::000:::::::0          4::::4  8::::::88888::::::8"<<endl;
+	cout<<"  2::::::2222222:::::2 00:::::::::::::00         44::::::44 88:::::::::::::88 "<<endl;
+	cout<<"  2::::::::::::::::::2   00:::::::::00           4::::::::4   88:::::::::88   "<<endl;
+	cout<<"  22222222222222222222     000000000             4444444444     888888888     "<<endl;
+	cout<<"                                                                              "<<endl;
+	cout<<"                                                                              "<<endl;
+	cout<<"                            An phim bat ki de tiep tuc                        "<<endl;
+	getch();
+	system("cls");
+	cout<<"                              Huong dan tro choi :                            "<<endl;
+	cout<<"                                                                              "<<endl;
+	cout<<"          Su dung mui ten "<<(char)1<<" de di chuyen                         "<<endl;
+	cout<<"          Su dung mui ten "<<(char)2<<" de di chuyen                         "<<endl;
+	cout<<"          Su dung mui ten "<<(char)3<<" de di chuyen                         "<<endl;
+	cout<<"          Su dung mui ten "<<(char)4<<" de di chuyen                         "<<endl;
+	cout<<"                                                                              "<<endl;
+	cout<<"                            An phim bat ki de tiep tuc                        "<<endl;
+	getch();
+	system("cls");
+}
+int main()
+{
+    introduction();
+}
