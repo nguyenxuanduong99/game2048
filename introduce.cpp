cb8acb26821a926b92402a068bd85b75305f0100
Thuong1998 <37398323+Thuong1998@users.noreply.github.com>
2018-03-16 13:47:35 +0700

    Add files via upload

---
+++ introduce.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
+using namespace std;
+void introduction()
+{
+    cout<<"                        CHAO MUNG BAN DEN VOI TRO CHOI                        "<<endl;
+    cout<<"                                                                              "<<endl;
+    cout<<"   222222222222222         000000000            444444444       888888888     "<<endl;system("color 0");Sleep(10);
+	cout<<"  2:::::::::::::::22     00:::::::::00         4::::::::4     88:::::::::88   "<<endl;system("color 1");Sleep(10);
+	cout<<"  2::::::222222:::::2  00:::::::::::::00      4:::::::::4   88:::::::::::::88 "<<endl;system("color 2");Sleep(10);
+	cout<<"  2222222     2:::::2 0:::::::000:::::::0    4::::44::::4  8::::::88888::::::8"<<endl;system("color 3");Sleep(10);
+	cout<<"              2:::::2 0::::::0   0::::::0   4::::4 4::::4  8:::::8     8:::::8"<<endl;system("color 4");Sleep(10);
+	cout<<"              2:::::2 0:::::0     0:::::0  4::::4  4::::4  8:::::8     8:::::8"<<endl;system("color 5");Sleep(10);
+	cout<<"           2222::::2  0:::::0     0:::::0 4::::4   4::::4   8:::::88888:::::8 "<<endl;system("color 6");Sleep(10);
+	cout<<"      22222::::::22   0:::::0 000 0:::::04::::444444::::444  8:::::::::::::8  "<<endl;system("color 8");Sleep(10);
+	cout<<"    22::::::::222     0:::::0 000 0:::::04::::::::::::::::4 8:::::88888:::::8 "<<endl;system("color 9");Sleep(10);
+	cout<<"   2:::::22222        0:::::0     0:::::04444444444:::::4448:::::8     8:::::8"<<endl;system("color A");Sleep(10);
+	cout<<"  2:::::2             0:::::0     0:::::0          4::::4  8:::::8     8:::::8"<<endl;system("color B");Sleep(10);
+	cout<<"  2:::::2             0::::::0   0::::::0          4::::4  8:::::8     8:::::8"<<endl;system("color C");Sleep(10);
+	cout<<"  2:::::2       2222220:::::::000:::::::0          4::::4  8::::::88888::::::8"<<endl;system("color D");Sleep(10);
+	cout<<"  2::::::2222222:::::2 00:::::::::::::00         44::::::44 88:::::::::::::88 "<<endl;system("color E");Sleep(10);
+	cout<<"  2::::::::::::::::::2   00:::::::::00           4::::::::4   88:::::::::88   "<<endl;system("color F");Sleep(10);
+	cout<<"  22222222222222222222     000000000             4444444444     888888888     "<<endl;system("color 2");Sleep(10);
+	cout<<"                                                                              "<<endl;
+	cout<<"                                                                              "<<endl;
+	cout<<"                            An phim bat ki de tiep tuc                        "<<endl;
+	getch();
+	system("cls");
+	system("color 9");
+	cout<<"                              Huong dan tro choi :                            "<<endl;
+	cout<<"                                                                              "<<endl;
+	cout<<"          Su dung mui ten "<<(char)24<<" de di chuyen                         "<<endl;
+	cout<<"          Su dung mui ten "<<(char)25<<" de di chuyen                         "<<endl;
+	cout<<"          Su dung mui ten "<<(char)26<<" de di chuyen                         "<<endl;
+	cout<<"          Su dung mui ten "<<(char)27<<" de di chuyen                         "<<endl;
+	cout<<"                                                                              "<<endl;
+	cout<<"                            An phim bat ki de tiep tuc                        "<<endl;
+	getch();
+	system("cls");
+	system("color 7");
+}
+int main()
+{
+    introduction();
+}
