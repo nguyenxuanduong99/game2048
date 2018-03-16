d0ec8c9c7f1da5ee1452bac84af77d749d14c3ba
Thuong1998 <37398323+Thuong1998@users.noreply.github.com>
2018-03-16 12:22:47 +0700

    Update introduce.cpp

--- introduce.cpp
+++ introduce.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 void introduction()
 {
-    cout<<"                        CHAO MUNG BAN DEN VOI TRO CHOI                        "<<endl;
-    cout<<"                                                                              "<<endl;
-    cout<<"   222222222222222         000000000            444444444       888888888     "<<endl;
+  	cout<<"                        CHAO MUNG BAN DEN VOI TRO CHOI                        "<<endl;
+	cout<<"                                                                              "<<endl;
+	cout<<"   222222222222222         000000000            444444444       888888888     "<<endl;
 	cout<<"  2:::::::::::::::22     00:::::::::00         4::::::::4     88:::::::::88   "<<endl;
 	cout<<"  2::::::222222:::::2  00:::::::::::::00      4:::::::::4   88:::::::::::::88 "<<endl;
 	cout<<"  2222222     2:::::2 0:::::::000:::::::0    4::::44::::4  8::::::88888::::::8"<<endl;
@@ -29,10 +29,10 @@ void introduction()
 	system("cls");
 	cout<<"                              Huong dan tro choi :                            "<<endl;
 	cout<<"                                                                              "<<endl;
-	cout<<"          Su dung mui ten "<<(char)24<<" de di chuyen                         "<<endl;
-	cout<<"          Su dung mui ten "<<(char)25<<" de di chuyen                         "<<endl;
-	cout<<"          Su dung mui ten "<<(char)26<<" de di chuyen                         "<<endl;
-	cout<<"          Su dung mui ten "<<(char)27<<" de di chuyen                         "<<endl;
+	cout<<"          Su dung mui ten "<<(char) 24<<" de di chuyen                         "<<endl;
+	cout<<"          Su dung mui ten "<<(char) 25<<" de di chuyen                         "<<endl;
+	cout<<"          Su dung mui ten "<<(char) 26<<" de di chuyen                         "<<endl;
+	cout<<"          Su dung mui ten "<<(char) 27<<" de di chuyen                         "<<endl;
 	cout<<"                                                                              "<<endl;
 	cout<<"                            An phim bat ki de tiep tuc                        "<<endl;
 	getch();
